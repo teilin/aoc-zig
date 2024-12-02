3b92cc0e9dd52c7b7507faab02e94ec9db04dc5c
Teis Lindemark <teis.lindemark@gmail.com>
2024-12-02 18:00:09 +0100

    Aoc part 2

--- src/solvers/2024/day02.zig
+++ src/solvers/2024/day02.zig
@@ -14,13 +14,14 @@ fn safeReport(allocator: std.mem.Allocator, content: []const u8, has_damper: boo
     while (readIter.next()) |line| {
         var words = std.mem.tokenizeSequence(u8, line, " ");
         var levels = std.ArrayList(i32).init(allocator);
+        defer levels.deinit();
         while (words.next()) |w| {
-            const iw = std.fmt.parseInt(i32, w, 10);
+            const iw = try std.fmt.parseInt(i32, w, 10);
             try levels.append(iw);
         }
 
         if (!has_damper) {
-            if (isSafe(levels, false, 0)) {
+            if (isSafe(levels.items, false, 0)) {
                 count += 1;
             }
         } else {
@@ -32,82 +33,33 @@ fn safeReport(allocator: std.mem.Allocator, content: []const u8, has_damper: boo
             }
         }
     }
+    return count;
 }
 
-fn isSafe(levels: []const i32, skip: bool, skip_id: usize) bool {
+fn isSafe(levels: []const i32, skip: bool, skip_index: usize) bool {
     var increment: bool = false;
     var decrement: bool = false;
     var diffCheck: bool = true;
     var prev: ?i32 = null;
     for (0.., levels) |index, level| {
-        if (skip and index == skip_id) {
+        if (skip and index == skip_index) {
             continue;
         }
         if (prev) |p| {
             const diff = @abs(level - p);
-            if (level - p < 0) {
+            if ((level - p) < 0) {
                 decrement = true;
             }
-            if (level - p > 0) {
+            if ((level - p) > 0) {
                 increment = true;
             }
-            if (diff > 0 and diff <= 3) {
-                diffCheck = true;
+            if (diff < 1 or diff > 3) {
+                diffCheck = false;
             }
         }
         prev = level;
     }
     return diffCheck and !(increment and decrement);
-}
-
-fn safeReportAnalyzer(content: []const u8, has_problem_damper: bool) !i32 {
-    var safeReports: i32 = 0;
-    var readIter = std.mem.tokenizeSequence(u8, content, "\n");
-
-    while (readIter.next()) |line| {
-        var lineIter = std.mem.tokenizeSequence(u8, line, " ");
-        var prevLevel: ?i32 = null;
-        var is_safe: bool = true;
-        var isIncreasing: ?bool = null;
-        while (lineIter.next()) |level| {
-            const levelInt: i32 = try std.fmt.parseInt(i32, level, 10);
-            if (prevLevel) |pl| {
-                if (isIncreasing == null) {
-                    if (pl > levelInt) {
-                        isIncreasing = false;
-                    } else {
-                        isIncreasing = true;
-                    }
-                }
-                const diff = @abs(levelInt - pl);
-                if (isIncreasing) |inc| {
-                    if (inc) {
-                        if (pl > levelInt) {
-                            is_safe = false;
-                        }
-                    } else {
-                        if (pl < levelInt) {
-                            is_safe = false;
-                        }
-                    }
-                }
-                if (diff > 0 and diff <= 3) {
-                    prevLevel = levelInt;
-                } else {
-                    is_safe = false;
-                }
-            } else {
-                prevLevel = levelInt;
-            }
-            prevLevel = levelInt;
-        }
-        if (has_problem_damper) {}
-        if (is_safe) {
-            safeReports += 1;
-        }
-    }
-
-    return safeReports;
 }
 
 test "part1 test" {
@@ -119,7 +71,7 @@ test "part1 test" {
         \\8 6 4 4 1
         \\1 3 6 7 9
     ;
-    try std.testing.expectEqual(2, safeReportAnalyzer(content, false));
+    try std.testing.expectEqual(2, safeReport(std.testing.allocator, content, false));
 }
 
 test "part2 test" {
@@ -131,5 +83,5 @@ test "part2 test" {
         \\8 6 4 4 1
         \\1 3 6 7 9
     ;
-    try std.testing.expectEqual(4, safeReportAnalyzer(content, true));
+    try std.testing.expectEqual(4, safeReport(std.testing.allocator, content, true));
 }
